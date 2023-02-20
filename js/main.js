b71cabead97ef8ca6cabea895c11f9fe30a37500
FeAnnG-devworks <fecelanng@gmail.com>
2023-02-20 23:40:43 +0700

    input main section in parts of home, about and popular

--- js/main.js
+++ js/main.js
@@ -29,6 +29,14 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click, linkAction'))
 
 // ============= Change background Header ================//
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header to the header tag
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       :header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
 
 
 
